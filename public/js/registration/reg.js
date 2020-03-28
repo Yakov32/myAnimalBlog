27e36addd751c3b940d8cb3b67081a68e317184e
Yakov32 <yakov.zverev32@gmail.com>
2020-03-29 00:04:56 +0300

    Images, which controller is saved.

--- public/js/registration/reg.js
+++ public/js/registration/reg.js
@@ -1,4 +1,47 @@
-let formRegister = document.getElementsByClassName('form-register')[0];
+$(document).ready( function(){
+    
+    $('.form-register').on('submit', function( event){
+        let login = $('.form-register .reg_login').val();
+        let pass  = $('.form-register .reg_pass').val();
+        let email = $('.form-register .reg_email').val();
+        let errorMessage = $('.form-register .reg_errorMessage');
+        
+        if(login.length < 6){
+            errorMessage.text('Min length of login is 6!');
+            event.preventDefault();
+               
+        }
+
+        else if(pass.length < 5){
+            errorMessage.text('Min length of password is 5!');
+            event.preventDefault();
+        }
+
+        else if(email.split('@').length-1 == 0){
+            errorMessage.text('Uncorrect Email!');
+            event.preventDefault();
+        }
+
+        /*else {
+            $.post(
+                "regist?cont_method=reg",
+                {
+                    "RegLogin": login,
+                    "Regpass": pass,
+                    "RegEmail": email
+                }
+            ); 
+        }*/
+    });
+
+});
+
+
+
+
+
+
+/*let formRegister = document.getElementsByClassName('form-register')[0];
 let reg_login = formRegister.RegLogin;
 let reg_pass = formRegister.RegPass;
 let reg_email = formRegister.RegEmail;
@@ -27,7 +70,7 @@ formRegister.onsubmit = (e) => {
         reg_pass.style.border = "none";
     }
     
-}
+}*/
 
 
 
